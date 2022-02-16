7dfa9981aa6082f6fb0689d2b06497e91996c45a
Reinhard Langner <ReinhardLangner@gmx.de>
2022-02-16 21:06:54 +0100

    update rdi data get

--- api/rkiFetch.ts
+++ api/rkiFetch.ts
@@ -20,11 +20,11 @@ export const rkiFetchDate = async () => {
 export const rkiGetNr = async (): Promise<number> => {
   try {
     const url =
-      "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?f=json&where=NeuerFall%20IN(1%2C%20-1)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22AnzahlFall%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&resultType=standard&cacheHint=true";
+      "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/rki_key_data_v/FeatureServer/0/query?f=json&where=1=1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=AdmUnitId asc&resultOffset=0&resultRecordCount=1&resultType=standard&cacheHint=true";
     const response = await fetch(url);
     if (response.ok) {
       const json = await response.json();
-      const val = json.features[0].attributes.value;
+      const val = json.features[0].attributes.AnzFallNeu;
       return parseInt(val);
     }
   } catch (err) {
@@ -36,11 +36,11 @@ export const rkiGetNr = async (): Promise<number> => {
 export const rkiGetDeath = async (): Promise<number> => {
   try {
     const url =
-      "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?f=json&where=NeuerTodesfall%20IN(1%2C%20-1)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22AnzahlTodesfall%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&resultType=standard&cacheHint=true";
+      "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/rki_key_data_v/FeatureServer/0/query?f=json&where=1=1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=AdmUnitId asc&resultOffset=0&resultRecordCount=1&resultType=standard&cacheHint=true";
     const response = await fetch(url);
     if (response.ok) {
       const json = await response.json();
-      const val = json.features[0].attributes.value;
+      const val = json.features[0].attributes.AnzTodesfallNeu;
       return parseInt(val);
     }
   } catch (err) {
